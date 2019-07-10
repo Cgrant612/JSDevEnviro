27aeff89bfee1469ae0248440df198597e218999
Connor Grant <connor.grant@ibm.com>
2019-07-10 08:27:28 -0500

    added API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import './index.css';
+import {getUsers} from './API/userApi';
 
-//import numeral from 'numeral';
+//Populate a table of users using an API call.
+getUsers().then(result => {
+  let usersBody = "";
 
-//const courseValue = numeral(1000).format('$0,0.00');
-//console.log(`I would pay ${courseValue} for this awesome course!`);
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+  global.document.getElementById('users').innerHTML = usersBody;
+});
